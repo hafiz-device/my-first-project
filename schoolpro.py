43eb082eb3d187490596e570123bd61a245bcb7d
issahak abdul-halim <issahaqabdulhalim@gmail.com>
2026-06-02 02:01:50 +0000

    SchoolPro Ghana - Initial menu

---
+++ schoolpro.py
@@ -0,0 +1,13 @@
+# ScoolPro Ghana
+# School Management Software
+# Developer: Issahak Abdul Halim(Hafiz)
+# Version: 1.0
+print("Welcome to SchoolPro Ghana!")
+print("School Management Software")
+print("===================================")
+print("1. Student Registration")
+print("2. Attendance")
+print("3. Grades & Results")
+print("4. Fee Management")
+print("5. Exit")
+print("===================================")
